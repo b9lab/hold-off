024ae8e717a931ea977159d6121044da4996aa12
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-03-09 11:58:14 +0000

    Moved to Truffle v3.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
+var HoldOff = artifacts.require("./HoldOff.sol");
+var StringStore = artifacts.require("./StringStore.sol");
+
 module.exports = function(deployer) {
-  deployer.deploy(HoldOff)
-  	.then(function () {
-  		return deployer.deploy(StringStore, HoldOff.address);
-  	});
+    deployer.deploy(HoldOff)
+        .then(() => deployer.deploy(StringStore, HoldOff.address));
 };
--- test/holdOff.js
+++ test/holdOff.js
@@ -1,86 +1,59 @@
-web3.eth.getTransactionReceiptMined = function (txnHash, interval) {
-  var transactionReceiptAsync;
-  interval = interval ? interval : 500;
-  transactionReceiptAsync = function(txnHash, resolve, reject) {
-    try {
-      var receipt = web3.eth.getTransactionReceipt(txnHash);
-      if (receipt == null) {
-        setTimeout(function () {
-          transactionReceiptAsync(txnHash, resolve, reject);
-        }, interval);
-      } else {
-        resolve(receipt);
-      }
-    } catch(e) {
-      reject(e);
-    }
-  };
+var HoldOff = artifacts.require("./HoldOff.sol");
+var StringStore = artifacts.require("./StringStore.sol");
 
-  return new Promise(function (resolve, reject) {
-      transactionReceiptAsync(txnHash, resolve, reject);
-  });
-};
-
-getEventsPromise = function (myFilter, count) {
-  return new Promise(function (resolve, reject) {
-    count = count ? count : 1;
-    var results = [];
-    myFilter.watch(function (error, result) {
-      if (error) {
-        reject(error);
-      } else {
-        count--;
-        results.push(result);
-      }
-      if (count <= 0) {
-        resolve(results);
-        myFilter.stopWatching();
-      }
-    });
-  });
-};
+Extensions = require("../utils/extensions.js");
+Extensions.init(web3, assert);
 
 contract('HoldOff', function(accounts) {
 
-  var owner = accounts[0];
-  var requester = accounts[1];
+    var owner, requester;
+    var holdOff, stringStore;
 
-  it("should be able to have delayed action", function() {
-    var holdOff = HoldOff.deployed();
-    var stringStore = StringStore.deployed();
-    var msgData;
+    before("should have correct accounts", () => {
+        assert.isAtLeast(accounts.length, 2, "should have at least 2 accounts");
+        owner = accounts[0];
+        requester = accounts[1];
+        return Extensions.makeSureAreUnlocked([ owner, requester ]);
+    });
 
-    return stringStore.myString()
-      .then(function (myString) {
-        assert.equal(myString, "", "should be an empty string to start with");
-        msgData = stringStore.contract.setString
-          .getData("Hello World", { from: requester });
+    beforeEach("should deploy both contracts", function() {
+        return HoldOff.new({ from: owner })
+            .then(created => {
+                holdOff = created;
+                return StringStore.new(holdOff.address, { from: owner });
+            })
+            .then(created => stringStore = created);
+    });
 
-        // Because we only want events from now on
-        var blockNumber = web3.eth.blockNumber;
+    it("should not be possible to update string if not the updater", function() {
+        return Extensions.expectedExceptionPromise(
+            () => stringStore.setString("Hello World", { from: requester, gas: 3000000 }),
+            3000000);
+    });
 
-        holdOff.requestCall(stringStore.address, msgData, { from: requester });
-        return getEventsPromise(holdOff.LogRequest({}, { from: web3.eth.blockNumber }));
-      })
-      .then(function (events) {
-        assert.equal(events.length, 1, "should have been a single event");
-        return holdOff.doCall(
-            events[0].args.target,
-            events[0].args.msgData,
-            { from: owner });
-      })
-      .then(function (tx) {
-        return web3.eth.getTransactionReceiptMined(tx);
-      })
-      .then(function (receipt) {
-        // Now has it been updated?
-        return stringStore.myString();
-      })
-      .then(function (myString) {
-        console.log(myString);
-        assert.equal(myString, "Hello World", "should have been updated");
-      });
+    it("should be possible to update string is passing through hold off", function() {
+        var msgData = stringStore.contract.setString.getData("Hello World");
 
-  });
+        return stringStore.myString()
+            .then(myString => {
+                assert.strictEqual(myString, "", "should be an empty string to start with");
+                return holdOff.requestCall(stringStore.address, msgData, { from: requester });
+            })
+            .then(txObject => {
+                assert.strictEqual(txObject.logs.length, 1, "should have been a single event");
+                var event0 = txObject.logs[0];
+                assert.strictEqual(event0.args.requester, requester, "should be the tx sender");
+                assert.strictEqual(event0.args.target, stringStore.address, "should be the string store");
+                assert.strictEqual(event0.args.msgData, msgData, "should be the msg data");
+                return holdOff.doCall(event0.args.target, event0.args.msgData, { from: owner });
+            })
+            .then(txObject => {
+                assert.strictEqual(txObject.logs.length, 0, "should have had no event");
+                // Now has it been updated?
+                return stringStore.myString();
+            })
+            .then(myString => assert.strictEqual(myString, "Hello World", "should have been updated"));
+
+    });
 
 });
